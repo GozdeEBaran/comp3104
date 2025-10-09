286e136e25ebc009f4db1f8f2d40ffe81f3e579d
GozdeEBaran <eski.gozdee@gmail.com>
2025-10-08 20:26:03 -0400

    week 6 update is added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101515982 || Gozde Baran || DevOps</h5>
+      <h5>Developed By: 101515982 || Gozde Baran || DevOps </h5>
+      <h5> week 6 update is done, it was really useful lecture!</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
